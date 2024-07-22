1af604df82a12bbab2f67d0d5349f50c5fadc1c5
Jordan Eizenga <joeizeng@gmail.com>
2024-07-22 16:55:02 -0700

    inline UF methods to make gcc happy

--- include/centrolign/union_find.hpp
+++ include/centrolign/union_find.hpp
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <cstdint>
+#include <algorithm>
 
 namespace centrolign {
 
@@ -21,13 +22,13 @@ public:
     UnionFind& operator=(UnionFind&& other) noexcept = default;
     
     // returns the group ID that index i belongs to (can change after calling union)
-    size_t find_group(size_t i);
+    inline size_t find_group(size_t i);
     
     // merges the group containing index i with the group containing index j
-    void union_groups(size_t i, size_t j);
+    inline void union_groups(size_t i, size_t j);
     
     // Returns all of the groups, each in a separate vector
-    std::vector<std::vector<size_t>> get_groups();
+    inline std::vector<std::vector<size_t>> get_groups();
     
 private:
     
@@ -39,6 +40,53 @@ private:
     };
     std::vector<Node> nodes;
 };
+
+inline size_t UnionFind::find_group(size_t i) {
+    std::vector<size_t> path;
+    // traverse tree upwards
+    while (nodes[i].head != i) {
+        path.push_back(i);
+        i = nodes[i].head;
+    }
+    // compress path
+    for (size_t p = 1; p < path.size(); p++) {
+        size_t j = path[p - 1];
+        nodes[j].head = i;
+    }
+    return i;
+}
+
+inline void UnionFind::union_groups(size_t i, size_t j) {
+    size_t head_i = find_group(i);
+    size_t head_j = find_group(j);
+    if (head_i != head_j) {
+        // use rank to determine which group to make the head
+        auto& node_i = nodes[head_i];
+        auto& node_j = nodes[head_j];
+        if (node_i.rank > node_j.rank) {
+            node_j.head = head_i;
+        }
+        else {
+            node_i.head = head_j;
+            
+            if (node_j.rank == node_i.rank) {
+                node_j.rank++;
+            }
+        }
+    }
+}
+
+inline std::vector<std::vector<size_t>> UnionFind::get_groups() {
+    std::vector<std::vector<size_t>> groups(nodes.size());
+    for (size_t i = 0; i < nodes.size(); i++) {
+        groups[find_group(i)].push_back(i);
+    }
+    auto new_end = std::remove_if(groups.begin(), groups.end(),
+                                  [](const std::vector<size_t>& grp) { return grp.empty(); });
+    groups.resize(new_end - groups.begin());
+    return groups;
+}
+
 
 }
 
